5d621d8219b27705de49399463bfe03ec63bde83
anvass <nastya_0809@mail.ru>
2024-11-13 20:34:11 +0300

    fix: adding require, fixing model

--- server/src/routers/wishes.api.router.js
+++ server/src/routers/wishes.api.router.js
@@ -1,3 +1,14 @@
-
-
 // npx sequelize model:generate --name Wish --attributes name:string,description:string,category:string,price:integer,isCompleted:boolean,userId:integer
+
+const router = require('express').Router();
+
+// router.get('/', verifyRefreshToken, (req, res) => {
+//   const { accessToken, refreshToken } = generateTokens({
+//     user: res.locals.user,
+//   });
+//   res
+//     .cookie('refreshToken', refreshToken, cookieConfig.refresh)
+//     .json({ user: res.locals.user, accessToken });
+// });
+
+module.exports = router;
